2b9d1d84e4250849f1d6bdc2b0c4c56bdb3fe04c
Haruni26 <aaron.seymour995@gmail.com>
2025-12-23 09:07:05 -0500

    Added Airbus logo to header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,3 +1,10 @@
+import Image from "next/image";
+import Logo from "../../public/images/logo.svg";
+
 export default function Header() {
-  return <h1>test</h1>;
+  return (
+    <div className="bg-gray-400/40 w-full p-6">
+      <Image src={Logo} alt="Pan Am Logo" width={190} priority />
+    </div>
+  );
 }
