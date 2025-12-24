61100aa966ea7e02a0c583857697dff4967c8d99
Haruni26 <aaron.seymour995@gmail.com>
2025-12-24 11:50:40 -0500

    Added Infinite slider with logos of carriers with the A220

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,59 @@
+"use client";
+
 import HeroVideo from "./components/heroVideo";
+import { InfiniteSlider } from "./components/motion-primitives/infinite-slider";
+import Image from "next/image";
+
 export default function Home() {
   return (
     <>
       <HeroVideo />
+      <InfiniteSlider className="p-5">
+        <Image
+          src="/images/ab-logo.png"
+          alt="Air Baltic Logo"
+          width={100}
+          height={150}
+          className="rounded-xl object-cover"
+        />
+        <Image
+          src="/images/ac-logo.png"
+          alt="Air Canada Logo"
+          width={100}
+          height={150}
+          className="rounded-xl object-cover"
+        />
+        <Image
+          src="/images/af-logo.png"
+          alt="Air France Logo"
+          width={100}
+          height={150}
+          className="rounded-xl object-cover"
+        />
+        <Image
+          src="/images/breeze-logo.png"
+          alt="Breeze Air Logo"
+          width={100}
+          height={150}
+          className="rounded-xl object-cover"
+        />
+        <Image
+          src="/images/delta-logo.png"
+          alt="Delta Airways Logo"
+          width={100}
+          height={150}
+          className="rounded-xl object-cover"
+        />
+        <Image
+          src="/images/jb-logo.png"
+          alt="Jetblue Logo"
+          width={100}
+          height={150}
+          className="rounded-xl object-cover"
+        />
+      </InfiniteSlider>
       <div>
-        <h1>test</h1>
+        <h1 className="p-6 text-2xl">Here&apos;s a deeper dive</h1>
       </div>
     </>
   );
